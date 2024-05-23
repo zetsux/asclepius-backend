1ad0d2deb701767b4ea65e6951afddf48371a8b7
Kevin Nathanael Halim <108170234+zetsux@users.noreply.github.com>
2024-05-23 12:09:20 +0700

    fix: adjust collection name

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -3,13 +3,13 @@ const { Firestore } = require('@google-cloud/firestore');
 async function storeData(id, data) {
   const db = new Firestore();
 
-  const predictCollection = db.collection('prediction');
+  const predictCollection = db.collection('predictions');
   return predictCollection.doc(id).set(data);
 }
 
 async function getData() {
   const db = new Firestore();
-  const collectionRef = db.collection('prediction');
+  const collectionRef = db.collection('predictions');
   const snapshot = await collectionRef.get();
 
   let data = [];
